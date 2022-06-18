b59b439092a99c919990e7adf123e63bef767c4b
brunnaserafina <brunnaserafina@gmail.com>
2022-06-18 03:55:02 -0300

    Número de cartas definido pelo usuário

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
+let quantidade = Number(prompt("Com quantas cartas você quer jogar? (Escolha um número par entre 4 e 14)"));
+
+const carta = `
+<li class="carta-escondida">
+<img src="./imgens/front.png">
+</li> `;
+
+
+while(quantidade%2 !== 0 || quantidade > 14 || quantidade < 4) {
+    quantidade = Number(prompt("Quantidade de cartas: "));
+}
+
+let contador = 0;
+
+while(contador < quantidade){
+    document.querySelector('ul').innerHTML += carta;
+    contador++;
+}
+
+
 
 
