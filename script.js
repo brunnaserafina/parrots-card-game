20083b634916e75d64f7b15a7b25f62c0524495b
brunnaserafina <brunnaserafina@gmail.com>
2022-06-18 23:39:12 -0300

    Cartas embaralhadas

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
+let quantidade;
+let gifs = [
+    "bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"
+];
+let embaralhar = [];
 
-let quantidade = Number(prompt("Com quantas cartas você quer jogar? (Escolha um número par entre 4 e 14)"));
-
-const carta = `
-<li class="carta-escondida">
-<img src="./imgens/front.png">
-</li> `;
-
-
-while(quantidade%2 !== 0 || quantidade > 14 || quantidade < 4) {
-    quantidade = Number(prompt("Quantidade de cartas: "));
+while (quantidade % 2 !== 0 || quantidade > 14 || quantidade < 4) {
+    quantidade = Number(prompt("Com quantas cartas você quer jogar? (Escolha um número par entre 4 e 14)"));
 }
 
-let contador = 0;
-
-while(contador < quantidade){
-    document.querySelector('ul').innerHTML += carta;
-    contador++;
+for (index = 0; index < (quantidade / 2); index ++) {
+    const papagaiosDancando = gifs[index];
+    embaralhar.push(papagaiosDancando);
+    embaralhar.push(papagaiosDancando);
 }
+
+embaralhar.sort(comparador);
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+for (index = 0; index < quantidade; index ++){
+    const listaembaralhada =
+    `
+    <li class="carta" onclick="virarCarta()">
+        
+        <img src="./imgens/${embaralhar[index]}.gif">
+    </li>
+    ` ;
+    
+    document.querySelector('ul').innerHTML += listaembaralhada; 
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
