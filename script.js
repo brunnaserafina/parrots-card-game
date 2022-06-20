13bb8e86ad3f81d656e1e98d6ab0fd128f8cffe7
brunnaserafina <brunnaserafina@gmail.com>
2022-06-20 03:54:09 -0300

    Correção: bloquear click de terceira carta em menos de 1s

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ embaralhar.sort(comparador);
 for (index = 0; index < quantidade; index++) {
     const listaembaralhada =
         `
-    <li class="card" id="${embaralhar[index]}" onclick="virarCartas(this)">
+    <li class="card" onclick="virarCartas(this)">
         <img class="front-face face" src="./imgens/${embaralhar[index]}parrot.gif">
         <img class="back-face face" src="./imgens/front.png">
     </li>
@@ -29,45 +29,56 @@ for (index = 0; index < quantidade; index++) {
     document.querySelector('ul').innerHTML += listaembaralhada;
 }
 
-let firstCard;
-let secondCard;
+let primeiraCarta;
+let segundaCarta;
 let contadorJogadas = 0;
 let contadorPares = 0;
+let bloquearCartas = false;
 
 function virarCartas(elemento) {
+    if(bloquearCartas){
+        return false;
+    }
+
     document.querySelectorAll(".card");
     elemento.classList.add("flip");
 
-    if (firstCard == undefined) {
-        firstCard = elemento;
+    if (primeiraCarta == undefined) {
+        primeiraCarta = elemento;
     } else {
-        secondCard = elemento;
+        segundaCarta = elemento;
+        comparar();
     }
-
-    comparar();
 }
 
 function comparar() {
-    if (firstCard.id !== secondCard.id) {
+    bloquearCartas = true;
+    if (primeiraCarta.innerHTML !== segundaCarta.innerHTML) {
         setTimeout(() => {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
-            firstCard = undefined;
-            secondCard = undefined;
+            primeiraCarta.classList.remove("flip");
+            segundaCarta.classList.remove("flip");
+            primeiraCarta = undefined;
+            segundaCarta = undefined;
+            bloquearCartas = false;
         }, 1000);
-        contadorJogadas+=2;
     } else {
-        firstCard = undefined;
-        secondCard = undefined;
-        contadorJogadas+=2;
+        primeiraCarta = undefined;
+        segundaCarta = undefined;
+        bloquearCartas = false;
         contadorPares++;
     }
 
-    if (contadorPares == (quantidade / 2)) {
+    contadorJogadas+=2;
+
+    if (contadorPares === (quantidade / 2)) {
         setTimeout(() => {
-        alert(`Você ganhou em ${contadorJogadas} jogadas!`)}, 1000);
+            alert(`Você ganhou em ${contadorJogadas} jogadas!`)
+        }, 1000);
     }
 }
+
+
+
 
 
 
